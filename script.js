d173847dfad8f90ea347b279aaceed701a676ee1
Premsai <premsaiellendula2525@gmail.com>
2024-11-03 08:40:43 +0530

    Renamed folder

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ async function checkWeather(city){
         document.querySelector(".weather").style.display = "none";
     }
     else{
-        var data = await response.json();
+        let data = await response.json();
 
 
         document.querySelector('.city').innerHTML = data.name;
